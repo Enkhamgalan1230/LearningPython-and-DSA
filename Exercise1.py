b944daa8362e7478939ea53dd0fdee99cd610116
Enkhamgalan1230 <115167095+Enkhamgalan1230@users.noreply.github.com>
2023-06-08 16:27:12 +0100

    first

---
+++ Exercise1.py
@@ -0,0 +1,64 @@
+# Exercise 1
+
+print("Hello, World!")
+
+# EXERCISE 2
+
+# change this code
+mystring = 'hello'
+myfloat = float(10)
+myint = 20
+
+# testing code
+if mystring == "hello":
+    print("String: %s" % mystring)
+if isinstance(myfloat, float) and myfloat == 10.0:
+    print("Float: %f" % myfloat)
+if isinstance(myint, int) and myint == 20:
+    print("Integer: %d" % myint)
+
+
+
+# EXERCISE 3
+numbers = []
+strings = []
+names = ["John", "Eric", "Jessica"]
+
+# write your code here
+
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+
+strings.append("hello")
+strings.append("world")
+
+second_name = names[1]
+
+
+# this code should write out the filled arrays and the second name in the names list (Eric).
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
+
+
+# Exercise 4
+
+x = object()
+y = object()
+
+# TODO: change this code
+x_list = [x]*10
+y_list = [y]*10
+big_list = x_list + y_list
+
+print("x_list contains %d objects" % len(x_list))
+print("y_list contains %d objects" % len(y_list))
+print("big_list contains %d objects" % len(big_list))
+
+# testing code
+if x_list.count(x) == 10 and y_list.count(y) == 10:
+    print("Almost there...")
+if big_list.count(x) == 10 and big_list.count(y) == 10:
+    print("Great!")
+
