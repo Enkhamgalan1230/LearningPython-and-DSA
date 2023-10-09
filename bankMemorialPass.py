180eaa2b6499b43febd85d5e7bc9cd0349c0ccf6
Enkhamgalan1230 <115167095+Enkhamgalan1230@users.noreply.github.com>
2023-10-09 17:03:16 +0100

    New

---
+++ bankMemorialPass.py
@@ -0,0 +1,7 @@
+name = "binderiya0820"
+
+num1 = int(input("First number: "))
+num2 = int(input("Second number: "))
+num3 = int(input("Third number: "))
+
+print("your memorial letters are", name[(num1)-1],name[(num2)-1],name[(num3)-1] )
